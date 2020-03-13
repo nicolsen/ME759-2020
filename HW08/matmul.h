93ea69c37ec4ca8d58b42b2d4ef73d744480b41d
Lijing Yang <ljyang1717@gmail.com>
2020-03-12 19:04:59 -0500

    HW08 provided files

---
+++ HW08/matmul.h
@@ -0,0 +1,18 @@
+// Author: Lijing Yang
+
+#ifndef MATMUL_H
+#define MATMUL_H
+
+#include <cstddef>
+#include <omp.h>
+
+// This function produces a parallel version of matrix multiplication C = A B using OpenMP. 
+// The resulting C matrix should be stored in row-major representation. 
+// Use the most optimal representation and order of operations as you found in 
+// HW02 task3. You may recycle the code from HW02.
+
+// The matrices A, B, and C have dimension n by n and are represented as 1D arrays.
+
+void mmul(const float* A, const float* B, float* C, const std::size_t n);
+
+#endif
